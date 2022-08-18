a2b531d6e8a5b97f90614b026e91dcda4f384c50
ssh <ssoohyun126@gmail.com>
2022-08-18 20:51:32 +0900

    swiftui

--- swiftDhc/ContentView.swift
+++ swiftDhc/ContentView.swift
@@ -10,73 +10,89 @@ import CoreData
 
 struct ContentView: View {
     @Environment(\.managedObjectContext) private var viewContext
-
+    
     @FetchRequest(
         sortDescriptors: [NSSortDescriptor(keyPath: \Item.timestamp, ascending: true)],
         animation: .default)
     private var items: FetchedResults<Item>
-
-//    var body: some View {
-//        NavigationView {
-//            List {
-//                ForEach(items) { item in
-//                    NavigationLink {
-//                        Text("Item at \(item.timestamp!, formatter: itemFormatter)")
-//                    } label: {
-//                        Text(item.timestamp!, formatter: itemFormatter)
-//                    }
-//                }
-//                .onDelete(perform: deleteItems)
-//            }
-//            .toolbar {
-//                ToolbarItem(placement: .navigationBarTrailing) {
-//                    EditButton()
-//                }
-//                ToolbarItem {
-//                    Button(action: addItem) {
-//                        Label("Add Item", systemImage: "plus")
-//                    }
-//                }
-//            }
-//            Text("Select an item")
-//        }
-//    }
+    
+    //    var body: some View {
+    //        NavigationView {
+    //            List {
+    //                ForEach(items) { item in
+    //                    NavigationLink {
+    //                        Text("Item at \(item.timestamp!, formatter: itemFormatter)")
+    //                    } label: {
+    //                        Text(item.timestamp!, formatter: itemFormatter)
+    //                    }
+    //                }
+    //                .onDelete(perform: deleteItems)
+    //            }
+    //            .toolbar {
+    //                ToolbarItem(placement: .navigationBarTrailing) {
+    //                    EditButton()
+    //                }
+    //                ToolbarItem {
+    //                    Button(action: addItem) {
+    //                        Label("Add Item", systemImage: "plus")
+    //                    }
+    //                }
+    //            }
+    //            Text("Select an item")
+    //        }
+    //    }
     
     var body: some View {
         TabView {
-            
-          Text("home")
-        
+            VStack{
+                Text("   상상쓰님, 환영합니다")
+                    .font(.system(size: 35).bold())
+                    .frame(maxWidth: .infinity, alignment: .leading)
+                Spacer()
+                Text("1")
+                Text("2")
+                Text("3")
+            }
             .tabItem {
-              Image(systemName: "house")
-              Text("home")
+                Image(systemName: "house")
+                Text("home")
+                
             }
             
-          Text("Another Tab")
-            .tabItem {
-              Image(systemName: "magnifyingglass")
-              Text("search")
+            VStack{
+                Text("Another Tab")
             }
-          Text("The Last Tab")
             .tabItem {
-              Image(systemName: "message")
-              Text("community")
+                Image(systemName: "magnifyingglass")
+                Text("search")
+                
             }
-            Text("The Last Tab")
-              .tabItem {
+            VStack{
+                Text("The Last Tab")
+            }
+            .tabItem {
+                Image(systemName: "message")
+                Text("community")
+                
+            }
+            VStack{
+                Text("The Last Tab")
+            }
+            .tabItem {
                 Image(systemName: "person")
                 Text("mypage")
-              }
-//            .badge(10)
+            }
+            //            .badge(10)
+            
         }
         .font(.headline)
-      }
-
+    }
+    
     private func addItem() {
         withAnimation {
             let newItem = Item(context: viewContext)
             newItem.timestamp = Date()
-
+            
             do {
                 try viewContext.save()
             } catch {
@@ -87,11 +103,11 @@ struct ContentView: View {
             }
         }
     }
-
+    
     private func deleteItems(offsets: IndexSet) {
         withAnimation {
             offsets.map { items[$0] }.forEach(viewContext.delete)
-
+            
             do {
                 try viewContext.save()
             } catch {
