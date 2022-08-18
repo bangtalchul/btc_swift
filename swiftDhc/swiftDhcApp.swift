df5e62c75345a2edd8085f3217b8f9c6480b4904
ssh <ssoohyun126@gmail.com>
2022-08-18 20:03:59 +0900

    Initial Commit

---
+++ swiftDhc/swiftDhcApp.swift
@@ -0,0 +1,20 @@
+//
+//  swiftDhcApp.swift
+//  swiftDhc
+//
+//  Created by 손수현 on 2022/08/18.
+//
+
+import SwiftUI
+
+@main
+struct swiftDhcApp: App {
+    let persistenceController = PersistenceController.shared
+
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+                .environment(\.managedObjectContext, persistenceController.container.viewContext)
+        }
+    }
+}
