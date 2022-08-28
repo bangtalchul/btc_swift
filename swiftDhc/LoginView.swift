7252a2dfe6d6607eed3b6ffe459e543ac9a6d072
ssh <ssoohyun126@gmail.com>
2022-08-28 18:12:59 +0900

    로그인뷰 파일만 추가

---
+++ swiftDhc/LoginView.swift
@@ -0,0 +1,20 @@
+//
+//  LoginView.swift
+//  swiftDhc
+//
+//  Created by 손수현 on 2022/08/28.
+//
+
+import SwiftUI
+
+struct LoginView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct LoginView_Previews: PreviewProvider {
+    static var previews: some View {
+        LoginView()
+    }
+}
