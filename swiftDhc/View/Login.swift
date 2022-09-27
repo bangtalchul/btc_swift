6c3bc94fc093fcc4a9b989d9aa45b92d0fa6e2f2
ssh <ssoohyun126@gmail.com>
2022-09-27 19:22:37 +0900

    로긴

--- swiftDhc/View/Login.swift
+++ swiftDhc/View/Login.swift
@@ -17,21 +17,48 @@ struct Login: View {
         VStack{
             Image("dhcLogo")
             
-            if let clientID = FirebaseApp.app()?.options.clientID{
-                GoogleSignInButton{
-                    GIDSignIn.sharedInstance.signIn(with: .init(clientID: clientID), presenting: UIApplication.shared.rootController()){user, error in
-                        if let error = error{
-                            print(error.localizedDescription)
-                            return
-                        }
-                        
-                        if let user = user{
-                            loginModel.logGoogleUser(user: user)
-                        }
-                        
-                    }
+            HStack(spacing: 8){
+                HStack{
+                    Image("logo")
+                        .resizable()
+                        .aspectRatio(contentMode: .fit)
+                        .frame(width: 25, height: 25)
+                        .frame(height: 45)
+                    
+                    Text("Google Sign In")
+                        .font(.callout)
+                        .lineLimit(1)
+                    
                 }
+                .foregroundColor(.white)
+                .padding(.horizontal, 15)
+                .background{
+                    RoundedRectangle(cornerRadius: 10, style: .continuous)
+                        .fill(.black)
+                }
+                .overlay{
+                    if let clientID = FirebaseApp.app()?.options.clientID{
+                        GoogleSignInButton{
+                            GIDSignIn.sharedInstance.signIn(with: .init(clientID: clientID), presenting: UIApplication.shared.rootController()){user, error in
+                                if let error = error{
+                                    print(error.localizedDescription)
+                                    return
+                                }
+                                
+                                if let user = user{
+                                    loginModel.logGoogleUser(user: user)
+                                }
+                                
+                            }
+                        }
+                                                .frame(height: 55)
+                                                .blendMode(.overlay)
+                    }
+
+                }
+                .clipped()
             }
+           
         HStack(spacing: 8){
             HStack{
                 Image("google")
